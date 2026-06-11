ee84073e81e10e6777d5ca461a81becd8dda9ac2
laysavic <laysa.alves8@aluno.ce.gov.br>
2026-06-11 04:11:52 -0300

    aumentando o timeout

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-
+    headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -77,6 +77,7 @@ export default defineConfig({
   //   command: 'npm run start',
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
-  // },
+  // },]
+  
 });
 
